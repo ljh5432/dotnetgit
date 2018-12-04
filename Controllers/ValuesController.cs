fabee41068ecd4400ee7902697fc074b954b4b44
지현 이 <ljh5432@gmail.com>
2018-12-04 12:29:57 +0900

    연습

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace dotnetgit.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "테스트", "value2" };
         }
 
         // GET api/values/5
